cab6d7e6cbbcc0e4ac598cb3e7db2b76dff7bece
Lance <1572534835@qq.com>
2021-07-16 16:21:18 +0800

    feat: 优化 enum 枚举方法

--- src/utils/enum/Enum.js
+++ src/utils/enum/Enum.js
@@ -7,27 +7,58 @@ export class Enum {
    * field: 枚举字段
    * label: 界面显示
    * value: 枚举值
+   * ext: 额外属性
    */
-  add(field, label, value) {
-    this[field] = {
-      label,
-      value,
-    };
+  add(field, label, value, ext = null) {
+    if (!ext) {
+      ext = {};
+      ext.className = field;
+    }
+    this[field] = { label, value, ext, field };
     return this;
   }
 
   /**
-   * 根据枚举value获取其label
+   * @description 根据枚举value获取其label
    */
   getLabelByValue(value) {
     // 字段不存在返回‘’
-    if (value === undefined || value === null) {
-      return "";
+    if (value === undefined || value === null) return "";
+    for (const [, val] of Object.entries(this)) {
+      if (val.value === value) {
+        return val.label;
+      }
     }
-    for (let i in this) {
-      let e = this[i];
-      if (e && e.value === value) {
-        return e.label;
+
+    return "";
+  }
+
+  /**
+   * @description 根据value找field
+   * @param value value值
+   */
+  getFieldByValue(value) {
+    // value不存在返回‘’
+    if (value === undefined || value === null) return "";
+    for (const [key, val] of Object.entries(this)) {
+      if (val.value === value) {
+        return key;
+      }
+    }
+
+    return "";
+  }
+
+  /**
+   * @description 根据field找value
+   * @param field field字段
+   */
+  getValueByField(field) {
+    // field不存在返回‘’
+    if (field === undefined || field === null) return "";
+    for (const [key, val] of Object.entries(this)) {
+      if (key === field) {
+        return val.value;
       }
     }
 
--- src/utils/enum/index.js
+++ src/utils/enum/index.js
@@ -19,4 +19,15 @@ export default {
   // ResultEnum: new Enum().add('SUCCESS', '操作成功', 200).add('ERROR', '操作失败', 400).add('PARAM_ERROR', '参数错误', 405).add(
   // 		'SERVER_ERROR', '服务器异常', 500)
   // 	.add('NO_PERMISSION', '没有权限', 501)
+  // mapEnum: new Enum()
+  //   // typeShow: 是否展示在编辑页的选择添加类型上
+  //   // show: 是否展示在库区图中
+  //   .add("input", "进料口", 2, { show: true, typeShow: true, fill: "rgba(16, 247, 191, 0.25)", activeFill: "rgba(163, 247, 191, 1)" })
+  //   .add("output", "出料口", 3, { show: true, typeShow: true, fill: "rgba(255, 245, 145, 0.25)", activeFill: "rgba(255, 245, 145, 1)" })
+  //   .add("area", "物料", 1, { show: true, typeShow: true, fill: "rgba(15, 255, 255, 0.25)", activeFill: "rgba(15, 255, 255, 1)" })
+  //   .add("wall", "墙", 0, { show: true, typeShow: true, fill: "rgba(232, 21, 255, 0.25)", activeFill: "rgba(232, 211, 255, 1)" })
+  //   .add("maintain", "检查口", 4, { show: true, typeShow: true, fill: "rgba(255, 150, 66, 0.25)", activeFill: "rgba(255, 150, 66, 1)" })
+  //   .add("mask", "遮罩", 5, { show: true, typeShow: false })
+  //   .add("warehouse", "库区", 6, { show: true, typeShow: false, fill: "rgba(95,149,255,0.05)", activeFill: "rgba(95,149,255, 1)" }),
+  // areaEnum: new Enum().add("Material", "物料区", 0).add("Seize", "可抓区", 1).add("Put", "可放区", 2),
 };
