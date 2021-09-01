9f72d4c8d502d2e6ec0259bd56a1e69d20215349
Lance <1572534835@qq.com>
2021-09-01 10:21:43 +0800

    feat: 防止按钮重复点击指令

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -8,6 +8,8 @@
     <div>{{ token }}</div>
     <!-- 组件 -->
     <sub-comp></sub-comp>
+
+    <el-button v-preventReClick="1000" type="primary" @click="handleClick">点我</el-button>
   </div>
 </template>
 
